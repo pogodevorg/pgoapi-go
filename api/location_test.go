3335e34b2a57c554f246213d9082c78d99530e04
femot <femot@me.com>
2016-11-12 00:04:39 +0100

    fix that stupid benchmark

--- api/location_test.go
+++ api/location_test.go
@@ -3,7 +3,7 @@ package api
 import "testing"
 
 func BenchmarkGetBytes(b *testing.B) {
-	l := Location{0.0, 0.0, 0.0}
+	l := Location{0.0, 0.0, 0.0, 0.0}
 	for n := 0; n < b.N; n++ {
 		l.GetBytes()
 	}
